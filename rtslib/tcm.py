open-iscsi/rtslib-fb

commit c5578a5597e62b682382d1890d02dbe78d87da2a

Fixed StorageObject deletion race.
* If StorageObject status update is too slow, we might go beyond the end of the
  attached LUNs list raising a StopIteration exception. Refactored to avoid
  that.
* Removed a couple of adjacent obsolete FIXMEs

rtslib/tcm.py
@@ -441,12 +441,11 @@ class StorageObject(CFSNode):
 
         # If we are called after a configure error, we can skip this
         if self.is_configured():
-            gen = self._gen_attached_luns()
-            while self.status == 'activated':
-                gen.next().delete()
-
-            # FIXME: delete ALUA tp pt gps
-            # FIXME: delete snapshots
+            for lun in self._gen_attached_luns():
+                if self.status != 'activated':
+                    break
+                else:
+                    gen.next().delete()
 
         super(StorageObject, self).delete()
 

commit 549d72078ffd05134bb318e1f08f91427c6b8dab

Fixed typo/regression on backstore deletion.
* Regression from c5578a5597e62b682382d1890d02dbe78d87da2a

rtslib/tcm.py
@@ -445,7 +445,7 @@ class StorageObject(CFSNode):
                 if self.status != 'activated':
                     break
                 else:
-                    gen.next().delete()
+                    lun.delete()
 
         super(StorageObject, self).delete()
 