8d25901c8da38a223d71b4e7abb6af0d2ada38d9
Andy Grover <agrover@redhat.com>
2012-12-20 10:16:43 -0800

    version 2.1.fb26
    Signed-off-by: Andy Grover <agrover@redhat.com>

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ import rtslib
 
 setup (
     name = 'rtslib',
-    version = '2.1.fb25',
+    version = '2.1.fb26',
     description = 'API for Linux kernel SCSI target (aka LIO)',
     license='AGPLv3',
     maintainer='Andy Grover',
