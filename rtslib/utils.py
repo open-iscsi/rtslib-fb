145e9b688cac46465fe876382e3d6e8125ab1ec9
Andy Grover <agrover@redhat.com>
2013-02-11 17:02:02 -0800

    Fix typo
    Signed-off-by: Andy Grover <agrover@redhat.com>

--- rtslib/utils.py
+++ rtslib/utils.py
@@ -41,7 +41,7 @@ class RTSLibNotInCFS(RTSLibError):
     '''
     The underlying configfs object does not exist. Happens when
     calling methods of an object that is instanciated but have
-    been deleted from congifs, or when trying to lookup an
+    been deleted from configfs, or when trying to lookup an
     object that does not exist.
     '''
     pass
