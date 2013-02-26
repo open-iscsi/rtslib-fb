a1c74eb4c44a97045934c0aa97413d80bf242a49
Andy Grover <agrover@redhat.com>
2013-02-26 13:09:23 -0800

    Update version to 2.1.fb30
    Signed-off-by: Andy Grover <agrover@redhat.com>

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ import rtslib
 
 setup (
     name = 'rtslib',
-    version = '2.1.fb26',
+    version = '2.1.fb30',
     description = 'API for Linux kernel SCSI target (aka LIO)',
     license='AGPLv3',
     maintainer='Andy Grover',
