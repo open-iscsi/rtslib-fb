open-iscsi/rtslib-fb

commit 608f1c47beef41dfa4774b352342f3151cee91e4

Don't lower-case return values from _parse_info functions
Shouldn't be necessary.

Signed-off-by: Andy Grover <agrover@redhat.com>

rtslib/tcm.py
@@ -167,7 +167,7 @@ class StorageObject(CFSNode):
         self._check_self()
         info = fread("%s/info" % self.path)
         return re.search(".*%s: ([^: ]+).*" \
-                         % key, ' '.join(info.split())).group(1).lower()
+                         % key, ' '.join(info.split())).group(1)
 
     def _get_status(self):
         self._check_self()
@@ -836,7 +836,7 @@ class _Backstore(CFSNode):
         self._check_self()
         info = fread("%s/hba_info" % self.path)
         return re.search(".*%s: ([^: ]+).*" \
-                         % key, ' '.join(info.split())).group(1).lower()
+                         % key, ' '.join(info.split())).group(1)
 
     def _get_version(self):
         self._check_self()

commit 11207ecc75dbd4917a3194216fcf5074122789a2

Fix delete regression
Introduced in 608f1c4. _parse_info no longer lowercases, so _get_status
must.

Fixes #53

Signed-off-by: Andy Grover <agrover@redhat.com>

rtslib/tcm.py
@@ -172,7 +172,7 @@ class StorageObject(CFSNode):
 
     def _get_status(self):
         self._check_self()
-        return self._parse_info('Status')
+        return self._parse_info('Status').lower()
 
     def _gen_attached_luns(self):
         '''