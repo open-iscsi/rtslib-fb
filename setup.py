54321cbe73366a81d03216e806d44350ebd11110
Andy Grover <agrover@redhat.com>
2015-05-26 14:27:27 -0700

    update to 2.1.fb56
    Signed-off-by: Andy Grover <agrover@redhat.com>

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup
 
 setup (
     name = 'rtslib-fb',
-    version = '2.1.55',
+    version = '2.1.56',
     description = 'API for Linux kernel SCSI target (aka LIO)',
     license = 'Apache 2.0',
     maintainer = 'Andy Grover',
