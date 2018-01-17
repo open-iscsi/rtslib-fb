open-iscsi/rtslib-fb

commit 2bcc4cd809409525bca62f09b08e97c2b97b0d43

Add support for xen-scsiback
Based on seeing https://github.com/Datera/rtslib/pull/10 we need to also
add a class to support this new fabric, since we don't use specs.

I can't test since Fedora doesn't include Xen dom0, but it *should* work.
:-)

Signed-off-by: Andy Grover <agrover@redhat.com>

rtslib/fabric.py
@@ -435,6 +435,14 @@ class VhostFabricModule(_BaseFabricModule):
         self.wwn_types = ('naa',)
         self.kernel_module = "tcm_vhost"
 
+class XenPvScsiFabricModule(_BaseFabricModule):
+    def __init__(self):
+        super(XenPvScsiFabricModule, self).__init__('xen_pvscsi')
+        self._path = "%s/%s" % (self.configfs_dir, 'xen-pvscsi')
+        self.features = ("nexus", "tpgts")
+        self.wwn_types = ('naa',)
+        self.kernel_module = "xen-scsiback"
+
 
 fabric_modules = {
     "srpt": SRPTFabricModule,
@@ -445,6 +453,7 @@ fabric_modules = {
     "tcm_fc": FCoEFabricModule,
 #    "usb_gadget": USBGadgetFabricModule, # very rare, don't show
     "vhost": VhostFabricModule,
+    "xen_pvscsi": XenPvScsiFabricModule,
     }
 
 #

commit 5a5670608b7abab061a59d65568e8ab0a53b6ef0

Correct name for Xen pvscsi
The name used in released products is xen-pvscsi, not xen_pvscsi.

Fixes: 2bcc4cd ("Add support for xen-scsiback")

Signed-off-by: Olaf Hering <olaf@aepfle.de>

rtslib/fabric.py
@@ -440,7 +440,7 @@ class VhostFabricModule(_BaseFabricModule):
 
 class XenPvScsiFabricModule(_BaseFabricModule):
     def __init__(self):
-        super(XenPvScsiFabricModule, self).__init__('xen_pvscsi')
+        super(XenPvScsiFabricModule, self).__init__('xen-pvscsi')
         self._path = "%s/%s" % (self.configfs_dir, 'xen-pvscsi')
         self.features = ("nexus", "tpgts")
         self.wwn_types = ('naa',)
@@ -469,7 +469,7 @@ fabric_modules = {
     "tcm_fc": FCoEFabricModule,
 #    "usb_gadget": USBGadgetFabricModule, # very rare, don't show
     "vhost": VhostFabricModule,
-    "xen_pvscsi": XenPvScsiFabricModule,
+    "xen-pvscsi": XenPvScsiFabricModule,
     "ibmvscsis": IbmvscsisFabricModule,
     }
 