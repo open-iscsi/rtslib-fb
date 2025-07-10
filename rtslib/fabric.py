open-iscsi/rtslib-fb

commit d3abd5df9e066e57e90182c2091b15451a0ff1cf

Convert codebase to pathlib

rtslib/fabric.py
@@ -108,6 +108,7 @@ Example: self._path = f"{self.configfs_dir}/{my_cfs_dir}"
 '''
 
 import os
+from contextlib import suppress
 from functools import partial
 from pathlib import Path
 
@@ -120,7 +121,6 @@ from .utils import (
     colonize,
     fread,
     fwrite,
-    ignored,
     modprobe,
     normalize_wwn,
 )
@@ -181,16 +181,15 @@ class _BaseFabricModule(CFSNode):
 
     def _list_targets(self):
         if self.exists:
-            for wwn in os.listdir(self.path):
-                if os.path.isdir(f"{self.path}/{wwn}") and \
-                        wwn not in target_names_excludes:
-                    yield Target(self, self.from_fabric_wwn(wwn), 'lookup')
+            for wwn in Path(self.path).iterdir():
+                if wwn.is_dir() and wwn.name not in target_names_excludes:
+                    yield Target(self, self.from_fabric_wwn(wwn.name), 'lookup')
 
     def _get_version(self):
         if self.exists:
             for attr in self.version_attributes:  # TODO
-                path = f"{self.path}/{attr}"
-                if os.path.isfile(path):
+                path = Path(self.path) / attr
+                if path.is_file():
                     return fread(path)
                 else:
                     raise RTSLibError(f"Can't find version for fabric module {self.name}")
@@ -379,10 +378,10 @@ class SBPFabricModule(_BaseFabricModule):
     # return 1st local guid (is unique) so our share is named uniquely
     @property
     def wwns(self):
-        for fname in glob("/sys/bus/firewire/devices/fw*/is_local"):
+        for fname in Path("/sys/bus/firewire/devices").glob("fw*/is_local"):
             if bool(int(fread(fname))):
-                guid_path = os.path.dirname(fname) + "/guid"
-                yield "eui." + fread(guid_path)[2:]
+                guid_path = fname.parent / "guid"
+                yield f"eui.{fread(guid_path)[2:]}"
                 break
 
 
@@ -402,13 +401,13 @@ class Qla2xxxFabricModule(_BaseFabricModule):
 
     @property
     def wwns(self):
-        for wwn_file in glob("/sys/class/fc_host/host*/port_name"):
-            with ignored(IOError):
-                host = os.path.realpath(os.path.dirname(wwn_file))
-                device = host.rsplit('/', 3)[0]
-                driver = os.path.basename(os.path.realpath(device+"/driver"))
-                if driver == "qla2xxx":
-                    yield "naa." + fread(wwn_file)[2:]
+        for wwn_file in Path("/sys/class/fc_host").glob("host*/port_name"):
+            with suppress(IOError):
+                host = Path(wwn_file).resolve().parent
+                device = host.parents[2]
+                driver = (device / "driver").resolve().name
+                if driver == "qla2xxx":  # TODO
+                    yield f"naa.{fread(wwn_file)[2:]}"
 
 
 class EfctFabricModule(_BaseFabricModule):
@@ -427,13 +426,13 @@ class EfctFabricModule(_BaseFabricModule):
 
     @property
     def wwns(self):
-        for wwn_file in glob("/sys/class/fc_host/host*/port_name"):
-            with ignored(IOError):
-                host = os.path.realpath(os.path.dirname(wwn_file))
-                device = host.rsplit('/', 3)[0]
-                driver = os.path.basename(os.path.realpath(device+"/driver"))
-                if driver == "efct":
-                    yield "naa." + fread(wwn_file)[2:]
+        for wwn_file in Path("/sys/class/fc_host").glob("host*/port_name"):
+            with suppress(IOError):
+                host = Path(wwn_file).resolve().parent
+                device = host.parents[2]
+                driver = (device / "driver").resolve().name
+                if driver == "efct":  # TODO
+                    yield f"naa.{fread(wwn_file)[2:]}"
 
 
 class SRPTFabricModule(_BaseFabricModule):
@@ -452,8 +451,8 @@ class SRPTFabricModule(_BaseFabricModule):
 
     @property
     def wwns(self):
-        for wwn_file in glob("/sys/class/infiniband/*/ports/*/gids/0"):
-            yield "ib." + fread(wwn_file).replace(":", "")
+        for wwn_file in Path("/sys/class/infiniband").glob("**/ports/*/gids/0"):
+            yield f"ib.{fread(wwn_file).replace(':', '')}"
 
 
 class FCoEFabricModule(_BaseFabricModule):
@@ -474,13 +473,13 @@ class FCoEFabricModule(_BaseFabricModule):
 
     @property
     def wwns(self):
-        for wwn_file in glob("/sys/class/fc_host/host*/port_name"):
-            with ignored(IOError):
-                host = os.path.realpath(os.path.dirname(wwn_file))
-                device = host.rsplit('/', 3)[0]
-                subsystem = os.path.basename(os.path.realpath(device+"/subsystem"))
+        for wwn_file in Path("/sys/class/fc_host").glob("host*/port_name"):
+            with suppress(IOError):
+                host = Path(wwn_file).resolve().parent
+                device = host.parents[2]
+                subsystem = (device / "subsystem").resolve().name
                 if subsystem == "fcoe":
-                    yield "naa." + fread(wwn_file)[2:]
+                    yield f"naa.{fread(wwn_file)[2:]}"
 
 
 class USBGadgetFabricModule(_BaseFabricModule):
@@ -515,7 +514,7 @@ class IbmvscsisFabricModule(_BaseFabricModule):
 
     @property
     def wwns(self):
-        for wwn_file in glob("/sys/module/ibmvscsis/drivers/vio:ibmvscsis/*/devspec"):
+        for wwn_file in Path("/sys/module/ibmvscsis/drivers/vio:ibmvscsis").glob("*/devspec"):
             name = fread(wwn_file)
             yield name[name.find("@") + 1:]
 

rtslib/node.py
@@ -18,8 +18,8 @@ License for the specific language governing permissions and limitations
 under the License.
 '''
 
-import os
 import stat
+from pathlib import Path
 
 from .utils import RTSLibError, RTSLibNotInCFSError, fread, fwrite
 
@@ -56,23 +56,25 @@ class CFSNode:
 
         if self.exists and mode == 'create':
             # ensure that self.path is not stale hba-only dir
-            if os.path.samefile(os.path.dirname(self.path), self.configfs_dir+'/core') \
-               and not next(os.walk(self.path))[1]:
-                os.rmdir(self.path)
+            path = Path(self._path)
+            if path.resolve().parent.samefile(Path(self.configfs_dir) / 'core') \
+                    and not any(path.iterdir()):
+                path.rmdir()
             else:
-               raise RTSLibError(f"This {self.__class__.__name__} already exists in configFS")
+                raise RTSLibError(f"This {self.__class__.__name__} already exists in configFS")
 
         elif not self.exists and mode == 'lookup':
-            raise RTSLibNotInCFSError(f"No such {self.__class__.__name__} in configfs: {self.path}")
+            raise RTSLibNotInCFSError(
+                f"No such {self.__class__.__name__} in configfs: {self.path}")
 
         if not self.exists:
             try:
-                os.mkdir(self.path)
-            except:
-                raise RTSLibError(f"Could not create {self.__class__.__name__} in configFS")
+                Path(self.path).mkdir()
+            except Exception as e:
+                raise RTSLibError(f"Could not create {self.__class__.__name__} in configFS: {e}")
 
     def _exists(self):
-        return os.path.isdir(self.path)
+        return Path(self.path).is_dir()
 
     def _check_self(self):
         if not self.exists:
@@ -95,25 +97,26 @@ class CFSNode:
         @return: List of file names filtered according to their
         read/write perms.
         '''
-        if not os.path.isdir(path):
+        path = Path(path)
+        if not path.is_dir():
             return []
 
         if writable is None and readable is None:
-            names = os.listdir(path)
+            names = [p.name for p in path.glob('*') if p.is_file()]
         else:
             names = []
-            for name in os.listdir(path):
-                sres = os.stat(f"{path}/{name}")
-                if (writable is not None and
-                        writable != ((sres[stat.ST_MODE] & stat.S_IWUSR) == stat.S_IWUSR)):
-                    continue
-                if (readable is not None and
-                        readable != ((sres[stat.ST_MODE] & stat.S_IRUSR) == stat.S_IRUSR)):
-                    continue
-                names.append(name)
+            for p in path.iterdir():
+                if p.is_file():
+                    sres = Path.stat(p)
+                    if (writable is not None and
+                            writable != ((sres[stat.ST_MODE] & stat.S_IWUSR) == stat.S_IWUSR)):
+                        continue
+                    if (readable is not None and
+                            readable != ((sres[stat.ST_MODE] & stat.S_IRUSR) == stat.S_IRUSR)):
+                        continue
+                    names.append(p.name)
 
-        names.sort()
-        return names
+        return sorted(names)
 
     # CFSNode public stuff
 
@@ -159,8 +162,8 @@ class CFSNode:
         @type value: string
         '''
         self._check_self()
-        path = f"{self.path}/attrib/{attribute!s}"
-        if not os.path.isfile(path):
+        path = Path(self.path) / 'attrib' / attribute
+        if not path.is_file():
             raise RTSLibError(f"Cannot find attribute: {attribute!s}")
         else:
             try:
@@ -174,8 +177,8 @@ class CFSNode:
         @return: The named attribute's value, as a string.
         '''
         self._check_self()
-        path = f"{self.path}/attrib/{attribute!s}"
-        if not os.path.isfile(path):
+        path = Path(self.path) / "attrib" / attribute
+        if not path.is_file():
             raise RTSLibError(f"Cannot find attribute: {attribute}")
         else:
             return fread(path)
@@ -190,8 +193,8 @@ class CFSNode:
         @type value: string
         '''
         self._check_self()
-        path = f"{self.path}/param/{parameter!s}"
-        if not os.path.isfile(path):
+        path = Path(self.path) / "param" / parameter
+        if not path.is_file():
             raise RTSLibError(f"Cannot find parameter: {parameter}")
         else:
             try:
@@ -206,8 +209,8 @@ class CFSNode:
         @return: The named parameter value as a string.
         '''
         self._check_self()
-        path = f"{self.path}/param/{parameter!s}"
-        if not os.path.isfile(path):
+        path = Path(self.path) / "param" / parameter
+        if not path.is_file():
             raise RTSLibError(f"Cannot find RFC-3720 parameter: {parameter}")
         else:
             return fread(path)
@@ -219,7 +222,7 @@ class CFSNode:
         to delete it.
         '''
         if self.exists:
-            os.rmdir(self.path)
+            Path(self.path).rmdir()
 
     path = property(_get_path,
             doc="Get the configFS object path.")

rtslib/root.py
@@ -19,7 +19,6 @@ under the License.
 '''
 
 import errno
-import glob
 import json
 import os
 from contextlib import suppress
@@ -157,16 +156,16 @@ class RTSRoot(CFSNode):
         return "rtslib"
 
     def _set_dbroot(self):
-        dbroot_path = self.path + "/dbroot"
-        if not os.path.exists(dbroot_path):
+        dbroot_path = Path(self.path) / "dbroot"
+        if not dbroot_path.exists():
             self._dbroot = self._default_dbroot
             return
         self._dbroot = fread(dbroot_path)
         if self._dbroot != self._preferred_dbroot:
             try:
-                fwrite(dbroot_path, self._preferred_dbroot+"\n")
+                fwrite(dbroot_path, f"{self._preferred_dbroot}\n")
             except:
-                if not os.path.isdir(self._preferred_dbroot):
+                if not Path(self._preferred_dbroot).is_dir():
                     raise RTSLibError(f"Cannot set dbroot to {self._preferred_dbroot}. "
                                       f"Please check if this directory exists.")
                 else:
@@ -191,7 +190,7 @@ class RTSRoot(CFSNode):
         current = self.dump()
 
         try:
-            with open(save_file) as f:
+            with Path(save_file).open as f:
                 saveconf = json.loads(f.read())
         except OSError as e:
             if e.errno == errno.ENOENT:
@@ -318,8 +317,8 @@ class RTSRoot(CFSNode):
         # If somehow some hbas still exist (no storage object within?) clean
         # them up too.
         if not (storage_object or target):
-            for hba_dir in glob.glob(f"{self.configfs_dir}/core/*_*"):
-                os.rmdir(hba_dir)
+            for hba_dir in Path(self.configfs_dir, 'core').glob('*_*'):
+                hba_dir.rmdir()
 
     def restore(self, config, target=None, storage_object=None,
                 clear_existing=False, abort_on_error=False):
@@ -456,24 +455,20 @@ class RTSRoot(CFSNode):
         umask = 0o777 ^ mode  # Prevents always downgrading umask to 0
 
         # For security, remove file with potentially elevated mode
-        with suppress(OSError):
-            tmp_file.unlink()
+        tmp_file.unlink(missing_ok=True)
 
         original_umask = os.umask(umask)
         # Even though the old file is first deleted, a race condition is still
         # possible. mode='x' opens the file for exclusive creation,
         # failing if the file already exists
         try:
-            with tmp_file.open(mode="x"):
-                pass
+            with tmp_file.open(mode="x") as f:
+                json.dump(saveconf, f, sort_keys=True, indent=2)
+                f.write("\n")
+                f.flush()
+                os.fsync(f.fileno())
         except OSError as e:
             raise RuntimeError(f"Could not open {tmp_file}") from e
-
-        with tmp_file.open(mode="w") as f:
-            json.dump(saveconf, f, sort_keys=True, indent=2)
-            f.write("\n")
-            f.flush()
-            os.fsync(f.fileno())
 
         # move along with permissions
         tmp_file.replace(save_file)
@@ -492,7 +487,7 @@ class RTSRoot(CFSNode):
         if not restore_file:
             restore_file = default_save_file
 
-        with open(restore_file) as f:
+        with Path(restore_file).open as f:
             config = json.loads(f.read())
             return self.restore(config, target, storage_object,
                                 clear_existing=clear_existing,

rtslib/target.py
@@ -22,7 +22,7 @@ import contextlib
 import os
 import uuid
 from functools import partial
-from glob import iglob as glob
+from pathlib import Path
 
 from . import tcm
 from .node import CFSNode
@@ -94,10 +94,9 @@ class Target(CFSNode):
 
     def _list_tpgs(self):
         self._check_self()
-        for tpg_dir in glob(f"{self.path}/tpgt*"):
-            tag = os.path.basename(tpg_dir).split('_')[1]
-            tag = int(tag)
-            yield TPG(self, tag, 'lookup')
+        for tpg_dir in Path(self.path).glob("tpgt*"):
+            tag = tpg_dir.name.split('_')[1]
+            yield TPG(self, int(tag), 'lookup')
 
     # Target public stuff
 
@@ -200,12 +199,13 @@ class TPG(CFSNode):
 
         self._path = "%s/tpgt_%d" % (self.parent_target.path, self.tag)
 
-        target_path = self.parent_target.path
-        if not self.has_feature('tpgts') and not os.path.isdir(self._path):
-            for filename in os.listdir(target_path):
-                if filename.startswith("tpgt_") \
-                   and os.path.isdir(f"{target_path}/{filename}") \
-                   and filename != "tpgt_%d" % self.tag:
+        path = Path(self._path)
+        target_path = Path(self.parent_target.path)
+        if not self.has_feature('tpgts') and not path.is_dir():
+            for filename in target_path.iterdir():
+                if filename.name.startswith("tpgt_") \
+                        and filename.is_dir() \
+                        and filename.name != f"tpgt_{self.tag}":
                     raise RTSLibError("Target cannot have multiple TPGs")
 
         self._create_in_cfs_ine(mode)
@@ -223,18 +223,17 @@ class TPG(CFSNode):
         if not self.has_feature('nps'):
             return
 
-        for network_portal_dir in os.listdir(f"{self.path}/np"):
-            (ip_address, port) = \
-                os.path.basename(network_portal_dir).rsplit(":", 1)
+        np_path = Path(self.path) / 'np'
+        for network_portal_dir in np_path.iterdir():
+            ip_address, port = network_portal_dir.name.rsplit(":", 1)
             port = int(port)
             yield NetworkPortal(self, ip_address, port, 'lookup')
 
     def _get_enable(self):
         self._check_self()
-        path = f"{self.path}/enable"
-        # If the TPG does not have the enable attribute, then it is always
-        # enabled.
-        if os.path.isfile(path):
+        path = Path(self.path) / 'enable'
+        # If the TPG does not have the enable attribute, then it is always enabled.
+        if path.is_file():
             return bool(int(fread(path)))
         else:
             return True
@@ -245,8 +244,8 @@ class TPG(CFSNode):
         attribute, do nothing.
         '''
         self._check_self()
-        path = f"{self.path}/enable"
-        if os.path.isfile(path) and (boolean != self._get_enable()):
+        path = Path(self.path) / 'enable'
+        if path.is_file() and (boolean != self._get_enable()):
             try:
                 fwrite(path, str(int(boolean)))
             except OSError as e:
@@ -293,8 +292,7 @@ class TPG(CFSNode):
         if not self.has_feature('acls'):
             return
 
-        node_acl_dirs = [os.path.basename(path)
-                         for path in os.listdir(f"{self.path}/acls")]
+        node_acl_dirs = [path.name for path in Path(self.path, 'acls').iterdir()]
         for node_acl_dir in node_acl_dirs:
             fm = self.parent_target.fabric_module
             yield NodeACL(self, fm.from_fabric_wwn(node_acl_dir), 'lookup')
@@ -315,8 +313,7 @@ class TPG(CFSNode):
 
     def _list_luns(self):
         self._check_self()
-        lun_dirs = [os.path.basename(path)
-                    for path in os.listdir(f"{self.path}/lun")]
+        lun_dirs = [path.name for path in Path(self.path, 'lun').iterdir()]
         for lun_dir in lun_dirs:
             lun = lun_dir.split('_')[1]
             lun = int(lun)
@@ -569,9 +566,9 @@ class LUN(CFSNode):
 
     def _get_alias(self):
         self._check_self()
-        for path in os.listdir(self.path):
-            if os.path.islink(f"{self.path}/{path}"):
-                return os.path.basename(path)
+        for path in Path(self.path).iterdir():
+            if path.is_symlink():
+                return path.name
 
         raise RTSLibBrokenLink("Broken LUN in configFS, no storage object")
 
@@ -642,9 +639,9 @@ class LUN(CFSNode):
         if self._get_storage_object().alua_supported is False:
             return None
 
-        path = f"{self.path}/alua_tg_pt_offline"
+        path = Path(self.path) / "alua_tg_pt_offline"
         try:
-            return bool(int(fread(path))) if os.path.isfile(path) else None
+            return bool(int(fread(path))) if path.is_file() else None
         except OSError:
             return None
 
@@ -654,9 +651,9 @@ class LUN(CFSNode):
         if self._get_storage_object().alua_supported is False:
             return -1
 
-        path = f"{self.path}/alua_tg_pt_offline"
+        path = Path(self.path) / "alua_tg_pt_offline"
         try:
-            if os.path.isfile(path):
+            if path.is_file():
                 fwrite(path, "1" if boolean else "0")
         except OSError:
             return -1
@@ -669,9 +666,9 @@ class LUN(CFSNode):
         if self._get_storage_object().alua_supported is False:
             return None
 
-        path = f"{self.path}/alua_tg_pt_status"
+        path = Path(self.path) / "alua_tg_pt_status"
         try:
-            return int(fread(path)) if os.path.isfile(path) else None
+            return int(fread(path)) if path.is_file() else None
         except OSError:
             return None
 
@@ -686,9 +683,9 @@ class LUN(CFSNode):
         if self._get_storage_object().alua_supported is False:
             return -1
 
-        path = f"{self.path}/alua_tg_pt_status"
+        path = Path(self.path) / "alua_tg_pt_status"
         try:
-            if os.path.isfile(path):
+            if path.is_file():
                 fwrite(path, str(integer))
         except OSError:
             return -1
@@ -701,9 +698,9 @@ class LUN(CFSNode):
         if self._get_storage_object().alua_supported is False:
             return None
 
-        path = f"{self.path}/alua_tg_pt_write_md"
+        path = Path(self.path) / "alua_tg_pt_write_md"
         try:
-            return bool(int(fread(path))) if os.path.isfile(path) else None
+            return bool(int(fread(path))) if path.is_file() else None
         except OSError:
             return None
 
@@ -713,9 +710,9 @@ class LUN(CFSNode):
         if self._get_storage_object().alua_supported is False:
             return -1
 
-        path = f"{self.path}/alua_tg_pt_write_md"
+        path = Path(self.path) / "alua_tg_pt_write_md"
         try:
-            if os.path.isfile(path):
+            if path.is_file():
                 fwrite(path, "1" if boolean else "0")
         except OSError:
             return -1
@@ -741,8 +738,9 @@ class LUN(CFSNode):
         except RTSLibBrokenLink:
             pass
         else:
-            if os.path.islink(f"{self.path}/{link}"):
-                os.unlink(f"{self.path}/{link}")
+            path = Path(self.path) / link
+            if path.is_symlink:
+                path.unlink()
 
         super().delete()
 
@@ -873,12 +871,12 @@ class NetworkPortal(CFSNode):
             return False
 
     def _set_iser(self, boolean):
-        path = f"{self.path}/iser"
+        path = Path(self.path) / "iser"
         try:
             fwrite(path, str(int(boolean)))
         except OSError:
             # b/w compat: don't complain if iser entry is missing
-            if os.path.isfile(path):
+            if path.is_file():
                 raise RTSLibError("Cannot change iser")
 
     def _get_offload(self):
@@ -889,12 +887,12 @@ class NetworkPortal(CFSNode):
             return False
 
     def _set_offload(self, boolean):
-        path = f"{self.path}/cxgbit"
+        path = Path(self.path) / "cxgbit"
         try:
             fwrite(path, str(int(boolean)))
         except OSError:
             # b/w compat: don't complain if cxgbit entry is missing
-            if os.path.isfile(path):
+            if path.is_file():
                 raise RTSLibError("Cannot change offload")
 
     # NetworkPortal public stuff
@@ -1020,8 +1018,8 @@ class NodeACL(CFSNode):
 
     def _list_mapped_luns(self):
         self._check_self()
-        for mapped_lun_dir in glob(f"{self.path}/lun_*"):
-            mapped_lun = int(os.path.basename(mapped_lun_dir).split("_")[1])
+        for mapped_lun_dir in Path(self.path).glob('lun_*'):
+            mapped_lun = int(mapped_lun_dir.name.split("_")[1])
             yield MappedLUN(self, mapped_lun)
 
     def _get_session(self):
@@ -1255,9 +1253,9 @@ class MappedLUN(CFSNode):
 
     def _get_alias(self):
         self._check_self()
-        for path in os.listdir(self.path):
-            if os.path.islink(f"{self.path}/{path}"):
-                return os.path.basename(path)
+        for path in Path(self.path).iterdir():
+            if path.is_symlink():
+                return path.name
 
         raise RTSLibBrokenLink("Broken LUN in configFS, no storage object")
 
@@ -1301,12 +1299,12 @@ class MappedLUN(CFSNode):
         '''
         self._check_self()
         try:
-            lun_link = f"{self.path}/{self.alias}"
+            lun_link = Path(self.path) / self.alias
         except RTSLibBrokenLink:
             pass
         else:
-            if os.path.islink(lun_link):
-                os.unlink(lun_link)
+            if lun_link.is_symlink():
+                lun_link.unlink()
         super().delete()
 
     mapped_lun = property(_get_mapped_lun,

commit 7f2ddbf5df9f5ff656b7f131eee301151711e7fd

fabric: fix search path for SRPT wwn
When converted to path link, Infiniband gid search path was replaced with a double wildcard '**'
causing no WWNs to be found and SRPT to be unusable:
No such path /srpt

This due to HCA in /sys/class/infiniband being symlinks:
$ ll /sys/class/infiniband
total 0
lrwxrwxrwx 1 root root 0 Jun 23 11:31 mlx5_0 -> ../../devices/pci0000:00/0000:00:02.6/0000:07:00.0/infiniband/mlx5_0

recurse_symlinks=True cannot be used here as HCA directories contains a 'subsystem' symlink
 pointing back to /sys/class/infiniband

Replace the double wildcard with a single one to revert to the previous behaviour and be
consistent with other fabrics.

Fixes: d3abd5df9e06 ("Convert codebase to pathlib")

Reviewed-by: Lee Duncan <lduncan@suse.com>
Signed-off-by: Nicolas Morey <nmorey@suse.com>

rtslib/fabric.py
@@ -451,7 +451,7 @@ class SRPTFabricModule(_BaseFabricModule):
 
     @property
     def wwns(self):
-        for wwn_file in Path("/sys/class/infiniband").glob("**/ports/*/gids/0"):
+        for wwn_file in Path("/sys/class/infiniband").glob("*/ports/*/gids/0"):
             yield f"ib.{fread(wwn_file).replace(':', '')}"
 
 